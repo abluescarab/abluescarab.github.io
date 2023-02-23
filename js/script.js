63d156230f2a8f88620d29d10788c67b264a608f
Alana Gilston <abluescarab@users.noreply.github.com>
2023-02-23 04:57:32 -0800

    Uncommented/fixed scroll code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 "use strict";
-// window.addEventListener("scroll", (e) => {
-//     const sections = document.querySelectorAll("section");
-//     const nav = document.querySelector("nav");
-//     for(const section of sections) {
-//         const top = section.offsetTop - (nav ? nav.scrollHeight : 0);
-//         const bottom = top + section.scrollHeight;
-//         if(window.scrollY >= top && window.scrollY <= bottom) {
-//             console.log(`in section ${section.id}`);
-//             document.querySelector("a.current")?.classList.remove("current");
-//             document.querySelector(`a[href='#${section.id}'`)?.classList.add("current");
-//         }
-//     }
-// });
+window.addEventListener("scroll", (e) => {
+    var _a, _b;
+    const sections = document.querySelectorAll("section");
+    const header = document.querySelector(".header-container");
+    for (const section of sections) {
+        const top = section.offsetTop - (header ? header.scrollHeight : 0);
+        const bottom = top + section.scrollHeight;
+        if (window.scrollY >= top && window.scrollY <= bottom) {
+            console.log(`in section ${section.id}`);
+            (_a = document.querySelector("a.current")) === null || _a === void 0 ? void 0 : _a.classList.remove("current");
+            (_b = document.querySelector(`a[href='#${section.id}'`)) === null || _b === void 0 ? void 0 : _b.classList.add("current");
+        }
+    }
+});
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,15 @@
-// window.addEventListener("scroll", (e) => {
-//     const sections = document.querySelectorAll("section");
-//     const nav = document.querySelector("nav");
+window.addEventListener("scroll", (e) => {
+    const sections = document.querySelectorAll("section");
+    const header = document.querySelector(".header-container");
 
-//     for(const section of sections) {
-//         const top = section.offsetTop - (nav ? nav.scrollHeight : 0);
-//         const bottom = top + section.scrollHeight;
+    for(const section of sections) {
+        const top = section.offsetTop - (header ? header.scrollHeight : 0);
+        const bottom = top + section.scrollHeight;
 
-//         if(window.scrollY >= top && window.scrollY <= bottom) {
-//             console.log(`in section ${section.id}`);
-//             document.querySelector("a.current")?.classList.remove("current");
-//             document.querySelector(`a[href='#${section.id}'`)?.classList.add("current");
-//         }
-//     }
-// });
+        if(window.scrollY >= top && window.scrollY <= bottom) {
+            console.log(`in section ${section.id}`);
+            document.querySelector("a.current")?.classList.remove("current");
+            document.querySelector(`a[href='#${section.id}'`)?.classList.add("current");
+        }
+    }
+});
